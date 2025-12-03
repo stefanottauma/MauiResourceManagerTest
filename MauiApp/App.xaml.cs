726be5eaa1d21afff064606977b9d6df7ccd0d44
stefanottauma <stefan.ott@auma.com>
2025-12-03 10:40:47 +0100

    upload project

---
+++ MauiApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace MauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ MauiApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using MauiApp.Resources.Texts;
+
+
+namespace MauiApp;
+
+public partial class MainPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		
+		var culture = new CultureInfo("de-DE");
+		Label.Text = Translations.ResourceManager.GetString("Text", culture) ?? string.Empty;
+	}
+
+	
+
+
+}
